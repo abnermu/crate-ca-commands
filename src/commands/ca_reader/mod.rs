78c6bfa20755df9fd13a626c422b65c24b8701e7
muyl <muyinlongkiss@163.com>
2024-11-28 18:23:43 +0800

    【muyl】 通用代码提炼

--- src/commands/ca_reader/mod.rs
+++ src/commands/ca_reader/mod.rs
@@ -1,20 +1,11 @@
-use std::{collections::HashMap, io::Cursor, time::Duration};
+use std::{io::Cursor, time::Duration};
 use base64::Engine;
 use image::GenericImageView;
 use serde::{Serialize, Deserialize};
-use hmac::Mac;
 use log as logger;
 
 use jyframe::utils::TokenUtil;
 
-const DEFAULT_ACCESS_SECRET: &str = "B32D22CABBB24963A42F10FFF49CF779";
-const DEFAULT_CLIENT_ID: &str = "Z0010020035";
-const DEFAULT_CLIENT_SECRET: &str = "D5BEA3E0F5A64BD19CB374C1876F1026";
-const DEFAULT_SERVICE_URL: &str = "http://218.60.154.155:8877/cashare/";
-const DEFAULT_TRADING_SYSTEM_CODE: &str = "X2100000027";
-const DEFAULT_SIGNATURE_SECRET: &str = "B32D22CABBB24963A42F10FFF49CF779";
-const DEFAULT_JWT_KEY: &str = "8784od7belusyfuw7oiq4i0mbzacxp32";
-const DEFAULT_JWT_TRADING_SYSTEM_KEY: &str = "8784od7belusyfuw7oiq4i0mbzacxp32";
 /// token返回结果对象
 #[derive(Default, Serialize, Deserialize)]
 pub struct TokenResult {
@@ -70,7 +61,7 @@ impl TokenFlag {
 pub fn ca_reader_token(flag: &str) -> Result<TokenResult, String> {
     let token_flag = TokenFlag::from_str(flag);
     let mut token_obj = TokenResult::default();
-    let mut expire_time = chrono::Utc::now();
+    let mut expire_time = chrono::Local::now();
     let token_info: Vec<&str> = token_flag.to_token().split("@@@").collect();
     token_obj.platformcode = token_info[0].to_string();
     let token_val = TokenUtil::create_key_secret_token_default(token_info[1], token_info[2]);
@@ -127,25 +118,25 @@ pub async fn ca_reader_proxy(method: &str, url: &str, options: serde_json::Value
 /// CA组件，中招扫码签章生成二维码
 #[tauri::command]
 pub async fn ca_reader_zz_qr_create() -> Result<serde_json::Value, String> {
-    Ok(zz_common_request(&zz_build_cebs().to_string(), "generateElectronicSealSignatureQRCode").await)
+    Ok(jyframe::ZhongZhaoUtil::zz_common_request(&jyframe::ZhongZhaoUtil::zz_build_cebs().to_string(), "generateElectronicSealSignatureQRCode").await)
 }
 /// CA组件，中招扫码签章查询二维码状态
 #[tauri::command]
 pub async fn ca_reader_zz_qr_status(dto: serde_json::Value) -> Result<serde_json::Value, String> {
     let body_obj = serde_json::json!({
         "tid": dto["tid"].as_str().unwrap_or(""),
-        "cebsSdkProperties": zz_build_cebs(),
+        "cebsSdkProperties": jyframe::ZhongZhaoUtil::zz_build_cebs(),
         "jwtProperties": serde_json::json!({
-            "key": DEFAULT_JWT_KEY,
-            "tradingSystemKey": DEFAULT_JWT_TRADING_SYSTEM_KEY,
+            "key": jyframe::ZhongZhaoUtil::DEFAULT_JWT_KEY,
+            "tradingSystemKey": jyframe::ZhongZhaoUtil::DEFAULT_JWT_TRADING_SYSTEM_KEY,
         }),
     });
-    Ok(zz_common_request(&body_obj.to_string(), "queryQRCodeScannedStatus").await)
+    Ok(jyframe::ZhongZhaoUtil::zz_common_request(&body_obj.to_string(), "queryQRCodeScannedStatus").await)
 }
 /// CA组件，中招扫码签章查询二维码结果
 #[tauri::command]
 pub async fn ca_reader_zz_qr_result(dto: serde_json::Value) -> Result<serde_json::Value, String> {
-    Ok(zz_common_request(&serde_json::json!({"tid": dto["tid"].as_str().unwrap_or("")}).to_string(), "getElectronicSealSignatureUserInfo").await)
+    Ok(jyframe::ZhongZhaoUtil::zz_common_request(&serde_json::json!({"tid": dto["tid"].as_str().unwrap_or("")}).to_string(), "getElectronicSealSignatureUserInfo").await)
 }
 /// CA组件，中招扫码签章查询二维码签章图片
 #[tauri::command]
@@ -157,54 +148,18 @@ pub async fn ca_reader_zz_qr_seals(dto: serde_json::Value) -> Result<serde_json:
         "orgTransactionCode": dto["orgTransactionCode"].as_str().unwrap_or(""),
         "personalTransactionCode": dto["personalTransactionCode"].as_str().unwrap_or(""),
         "accessToken": dto["accessToken"].as_str().unwrap_or(""),
-        "tradingSystemCode": DEFAULT_TRADING_SYSTEM_CODE,
+        "tradingSystemCode": jyframe::ZhongZhaoUtil::DEFAULT_TRADING_SYSTEM_CODE,
         "orgCode": dto["orgCode"].as_str().unwrap_or(""),
         "idCardHash": dto["idCardHash"].as_str().unwrap_or(""),
     });
-    let mut rtn = zz_common_request(&body_obj.to_string(), "getUserSeals").await;
+    let mut rtn = jyframe::ZhongZhaoUtil::zz_common_request(&body_obj.to_string(), "getUserSeals").await;
     if let Some(rtn_data) = rtn["data"].as_array_mut() {
         if rtn_data.len() > 0 {
+            // 图片缩放
             let max_width: f64 = 150.0 * (dto["dpi"].as_f64().unwrap_or(96.0)) / 72.0;
             for img_obj in rtn_data {
                 if let Some(seal_img) = img_obj["sealImage"].as_str() {
-                    match base64::engine::general_purpose::STANDARD.decode(seal_img) {
-                        Ok(img_bytes) => {
-                            match image::ImageReader::new(Cursor::new(&img_bytes[..])).with_guessed_format() {
-                                Ok(img_reader) => {
-                                    match img_reader.decode() {
-                                        Ok(mut img_ins) => {
-                                            let (ori_width, ori_height) = img_ins.dimensions();
-                                            if ori_width as f64 > max_width {
-                                                let scale: f64 = max_width / ori_width as f64;
-                                                let scaled_width = ori_width as f64 * scale;
-                                                let scaled_height = ori_height as f64 * scale;
-                                                img_ins = img_ins.resize(scaled_width as u32, scaled_height as u32, image::imageops::FilterType::Lanczos3);
-                                                let mut scaled_img_bytes: Vec<u8> = Vec::new();
-                                                match img_ins.write_to(&mut Cursor::new(&mut scaled_img_bytes), image::ImageFormat::Png) {
-                                                    Ok(_) => img_obj["sealImage"] = serde_json::json!(base64::engine::general_purpose::STANDARD.encode(&scaled_img_bytes[..])),
-                                                    Err(err) => {
-                                                        logger::error!("write resized image to bytes failed: {}", err);
-                                                    }
-                                                }
-                                            }
-                                            else {
-                                                logger::info!("the seal image no need to be resized");
-                                            }
-                                        },
-                                        Err(err) => {
-                                            logger::error!("get dynamic image instance failed: {}", err);
-                                        }
-                                    }
-                                },
-                                Err(err) => {
-                                    logger::error!("create image reader failed: {}", err);
-                                }
-                            }
-                        },
-                        Err(err) => {
-                            logger::error!("try to convert sealImage from base64 to bytes failed: {}", err);
-                        }
-                    }
+                    img_obj["sealImage"] = serde_json::json!(resize_seal_image(seal_img, max_width));
                 }
                 else {
                     logger::warn!("there is not user sealImage");
@@ -240,94 +195,45 @@ fn get_url_real(url: &str) -> String {
     }
     url.to_string()
 }
-/// 内部方法，中招通用请求
-async fn zz_common_request(body: &str, feature_code: &str) -> serde_json::Value {
-    let headers = zz_make_headers(DEFAULT_CLIENT_ID, DEFAULT_CLIENT_SECRET, DEFAULT_TRADING_SYSTEM_CODE, 
-        feature_code, "electronicSealSignature", "V1.0.0", body, DEFAULT_SIGNATURE_SECRET);
-    let client = reqwest::Client::new();
-    let mut form = HashMap::new();
-    form.insert("businessData", body);
-    logger::info!("request to zz server, the headers is {:?} and the body is {:?}", &headers, &form);
-    match client.post(format!("{}/CAShare/Components", DEFAULT_SERVICE_URL)).headers(headers).form(&form).send().await {
-        Ok(res) => {
-            match res.json::<serde_json::Value>().await {
-                Ok(rtn) => {
-                    return rtn.clone();
+/// 内部方法图片缩放
+fn resize_seal_image(seal_img: &str, max_width: f64) -> String {
+    match base64::engine::general_purpose::STANDARD.decode(seal_img) {
+        Ok(img_bytes) => {
+            match image::ImageReader::new(Cursor::new(&img_bytes[..])).with_guessed_format() {
+                Ok(img_reader) => {
+                    match img_reader.decode() {
+                        Ok(mut img_ins) => {
+                            let (ori_width, ori_height) = img_ins.dimensions();
+                            if ori_width as f64 > max_width {
+                                let scale: f64 = max_width / ori_width as f64;
+                                let scaled_width = ori_width as f64 * scale;
+                                let scaled_height = ori_height as f64 * scale;
+                                img_ins = img_ins.resize(scaled_width as u32, scaled_height as u32, image::imageops::FilterType::Lanczos3);
+                                let mut scaled_img_bytes: Vec<u8> = Vec::new();
+                                match img_ins.write_to(&mut Cursor::new(&mut scaled_img_bytes), image::ImageFormat::Png) {
+                                    Ok(_) => return base64::engine::general_purpose::STANDARD.encode(&scaled_img_bytes[..]),
+                                    Err(err) => {
+                                        logger::error!("write resized image to bytes failed: {}", err);
+                                    }
+                                }
+                            }
+                            else {
+                                logger::info!("the seal image no need to be resized");
+                            }
+                        },
+                        Err(err) => {
+                            logger::error!("get dynamic image instance failed: {}", err);
+                        }
+                    }
                 },
                 Err(err) => {
-                    logger::error!("try to convert zz response to json object failed: {}", err);
+                    logger::error!("create image reader failed: {}", err);
                 }
             }
         },
         Err(err) => {
-            logger::error!("try to request zz server {} failed: {}", format!("{}/CAShare/Components", DEFAULT_SERVICE_URL), err);
+            logger::error!("try to convert sealImage from base64 to bytes failed: {}", err);
         }
     }
-    serde_json::json!({})
-}
-/// 内部方法，中招生成通用请求header
-fn zz_make_headers(client_id: &'static str, client_secret: &'static str, trading_system_code: &'static str, 
-                   feature_code: &str, service_code: &'static str, version: &'static str, 
-                   body: &str, signature_secret: &'static str) -> reqwest::header::HeaderMap {
-    let curr_time = chrono::Utc::now();
-    let time_stamp = curr_time.timestamp_millis().to_string();
-    let request_uuid = uuid::Uuid::new_v4();
-    let authorization = format!("Basic {}", base64::engine::general_purpose::STANDARD.encode(format!("{}:{}", client_id, client_secret).as_bytes()));
-    let mut headers = reqwest::header::HeaderMap::new();
-    headers.append("x-sdk-invoke-type", reqwest::header::HeaderValue::from_static("common"));
-    headers.append("ServiceCode", reqwest::header::HeaderValue::from_static(service_code));
-    if let Ok(hv_feature_code) = reqwest::header::HeaderValue::from_str(feature_code) {
-        headers.append("FeatureCode", hv_feature_code);
-    }
-    headers.append("Version", reqwest::header::HeaderValue::from_static(version));
-    headers.append("TradingSystemCode", reqwest::header::HeaderValue::from_static(trading_system_code));
-    if let Ok(hv_timestamp) = reqwest::header::HeaderValue::from_str(&time_stamp) {
-        headers.append("Timestamp", hv_timestamp);
-    }
-    if let Ok(hv_request_uuid) = reqwest::header::HeaderValue::from_str(&request_uuid.to_string()) {
-        headers.append("Nonce", hv_request_uuid);
-    }
-    if let Ok(hv_authorization) = reqwest::header::HeaderValue::from_str(&authorization) {
-        headers.append("Authorization", hv_authorization);
-    }
-    if let Ok(hv_signature) = reqwest::header::HeaderValue::from_str(
-        &zz_cal_signature(body, &time_stamp, &request_uuid.to_string(), &authorization, feature_code, service_code, version, signature_secret)
-    ) {
-        headers.append("Signature", hv_signature);
-    }
-    headers
-}
-/// 内部方法，计算中招的请求签名
-fn zz_cal_signature(body: &str, time_stamp: &str, request_uuid: &str, authorization: &str, feature_code: &str, service_code: &str, version: &str, signature_secret: &str) -> String {
-    let mut vec_message: Vec<String> = vec![];
-    vec_message.push(format!("{}={}", "Authorization", authorization));
-    vec_message.push(format!("{}={}", "FeatureCode", feature_code));
-    vec_message.push(format!("{}={}", "Nonce", request_uuid));
-    vec_message.push(format!("{}={}", "ServiceCode", service_code));
-    vec_message.push(format!("{}={}", "Timestamp", time_stamp));
-    vec_message.push(format!("{}={}", "Version", version));
-    vec_message.push(format!("{}={}", "businessData", body));
-    let message = format!("{}{}{}", vec_message.join(","), &time_stamp[0..3], &request_uuid[0..3]);
-    match hmac::Hmac::<sm3::Sm3>::new_from_slice(signature_secret.as_bytes()) {
-        Ok(mut hmac) => {
-            hmac.update(message.as_bytes());
-            let rtn = hex::encode(hmac.finalize().into_bytes());
-            return rtn;
-        },
-        Err(err) => {
-            logger::error!("hmac-sm3 hash failed: {}", err);
-        }
-    }
-    String::from("")
-}
-/// 内部方法，生成cebs属性对象
-fn zz_build_cebs() -> serde_json::Value {
-    serde_json::json!({
-        "accessKeySecret": DEFAULT_ACCESS_SECRET,
-        "clientId": DEFAULT_CLIENT_ID,
-        "clientSecret": DEFAULT_CLIENT_SECRET,
-        "tradingSystemCode": DEFAULT_TRADING_SYSTEM_CODE,
-        "serviceUrl": DEFAULT_SERVICE_URL,
-        "signatureSecret": DEFAULT_SIGNATURE_SECRET,
-    })
+    seal_img.to_string()
 }
